2e6a889f5443b1480bcab03d2b66c3cb3d4f39bd
Robert Buchi <robert@planetweb.ro>
2025-02-15 13:40:51 +0200

    'v1.0.1'

--- electron/app/Routes/routes.js
+++
@@ -1,4 +0,0 @@
-Route.get('/main/get-config', 'Main/GetConfigAction');
-Route.post('/main/patch-config', 'Main/PatchConfigAction');
-Route.post('/main/toggle-tray', 'Main/ToggleTrayAction');
-Route.get('/main/open-folder', 'Main/OpenFolderAction');
